eefc0575018d83aa7172292c9ea41404c0995370
Mattis Erkensten <m.erkensten@icloud.com>
2022-02-14 20:07:00 +0100

    video 5 klar

---
+++ components/RecipeCard.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export const RecipeCard = ({ recipe }) => {
+  const { title, slug, cookingTime, thumbnail } = recipe.fields;
+  return (
+    <div className="card">
+      <div className="featured">
+        <Image
+          src={'https:' + thumbnail.fields.file.url}
+          width={thumbnail.fields.file.details.image.width}
+          height={thumbnail.fields.file.details.image.height}
+        ></Image>
+      </div>
+      <div className="content">
+        <div className="info">
+          <h4>{title}</h4>
+          <p>Takes approx {cookingTime} to make</p>
+        </div>
+        <div className="actions">
+          <Link href={`/recipes/${slug}`}>
+            <a>Cook this</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
