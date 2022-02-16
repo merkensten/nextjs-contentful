408f7b4406727facd54d53ad426aefecb54a5135
Mattis Erkensten <m.erkensten@icloud.com>
2022-02-16 14:09:47 +0100

    fallback skapad

--- pages/recipes/[slug].jsx
+++ pages/recipes/[slug].jsx
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful';
 import Image from 'next/image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { Skeleton } from '../../components/Skeleton';
 
 const client = createClient({
   space: process.env.CONTENFUL_SPACE_ID,
@@ -16,7 +17,7 @@ export const getStaticPaths = async () => {
     };
   });
 
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 };
 
 export const getStaticProps = async ({ params }) => {
@@ -32,6 +33,11 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export default function RecipeDetails({ recipe }) {
+
+  if (!recipe) {
+    return <Skeleton />;
+  }
+
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
 
